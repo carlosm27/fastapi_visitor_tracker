909de0ee377a11c4b1443e72265cc80643648e7d
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-07-21 19:40:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,27 +6,20 @@ from robyn.robyn import Response
 from controllers import all_logs, new_log
 import json    
 import asyncio
-import aio_pika  
+import aio_pika
+from consumer import on_message
 
 app = FastAPI()
 
 @app.on_event('startup')
 async def startup():
     loop = asyncio.get_event_loop()
-    conn = await aio_pika.connect_robust('amqp://guest:guest@localhost:5672', loop=loop)
-    channel = await conn.channel()
-    queue = await channel.declare_queue('logs')
+    connection = await aio_pika.connect("amqp://guest:guest@localhost/", loop = loop)
+    channel =  await connection.channel()
+    queue = await channel.declare_queue("logs")
+    await queue.consume(on_message)
 
-    
-    def consumer(message: aio_pika.Message):
-        print(message.body)
-        txt = message.body.decode("utf-8")
-        tracker = json.loads(txt)
-        new_log(tracker["ip_address"], tracker["request_url"], tracker["request_port"],
-                        tracker["request_path"], tracker["request_method"],
-                        tracker["browser_type"], tracker["operating_system"],tracker["request_time"])
 
-    await queue.consume(consumer)
 
 
 @app.get("/message")
