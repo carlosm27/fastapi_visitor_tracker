cfdfb4fcf1b8d8b7b2faee64d7d6b75290d54843
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-07-21 19:40:43 -0400

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -10,22 +10,8 @@ async def on_message(message: aio_pika.IncomingMessage):
     tracker = json.loads(txt)
     new_log(tracker["ip_address"], tracker["request_url"], tracker["request_port"],
                         tracker["request_path"], tracker["request_method"],
-                        tracker["browser_type"], tracker["operating_system"],tracker["request_time"])
+                        tracker["browser_type"], tracker["operating_system"],tracker["request_time"], tracker["service_name"])
     print(txt)
 
-async def receiver(loop):
-    connection = await aio_pika.connect("amqp://guest:guest@localhost/", loop = loop)
-    channel =  await connection.channel()
-    queue = await channel.declare_queue("logs")
-        
-       
-    await queue.consume(on_message, no_ack=True)
-    print(' [*] Waiting for messages.')
-        
-    
-  
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.create_task(receiver(loop))
-    loop.run_forever()
+
 
