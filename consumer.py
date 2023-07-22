ff3461f22e31beec83fbfda98225258f1c1ef197
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-07-22 19:51:25 -0400

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -4,14 +4,17 @@ from controllers import new_log
 import json    
 import asyncio
 import aio_pika  
+import ast
 
 async def on_message(message: aio_pika.IncomingMessage):
-    txt = message.body.decode("utf-8")
-    tracker = json.loads(txt)
+    tracker = ast.literal_eval(message.body.decode("utf-8"))
+    print(type(tracker))
+    #tracker = json.loads(txt)
+    print(type(tracker))
     new_log(tracker["ip_address"], tracker["request_url"], tracker["request_port"],
                         tracker["request_path"], tracker["request_method"],
-                        tracker["browser_type"], tracker["operating_system"],tracker["request_time"], tracker["service_name"])
-    print(txt)
+                        tracker["browser_type"],tracker["request_time"], tracker["service_name"])
+    print(tracker)
 
 
 
